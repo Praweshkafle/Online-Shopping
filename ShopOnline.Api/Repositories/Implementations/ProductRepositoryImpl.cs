e0cdcee8c36ff9e9a9dfcb9994f887de93ad043a
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2023-10-05 20:48:08 +0545

    added database connection and implemented repository pattern

---
+++ ShopOnline.Api/Repositories/Implementations/ProductRepositoryImpl.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Entity;
+using ShopOnline.Api.Repositories.Interfaces;
+
+namespace ShopOnline.Api.Repositories.Implementations
+{
+    public class ProductRepositoryImpl : ProductRepository
+    {
+        private readonly AppDbContext _context;
+        public ProductRepositoryImpl(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            var categories= await _context.ProductCategories.ToListAsync();
+            return categories;
+        }
+
+        public Task<Product> GetCategory(int id)
+        {
+            return default;
+        }
+
+        public Task<Product> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetItems()
+        {
+            var products = await _context.Products.ToListAsync();
+            return products;
+        }
+    }
+}
---
+++ ShopOnline.Api/Repositories/Interfaces/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using ShopOnline.Api.Entity;
+
+namespace ShopOnline.Api.Repositories.Interfaces
+{
+    public interface ProductRepository
+    {
+        Task<IEnumerable<Product>> GetItems();
+        Task<IEnumerable<ProductCategory>> GetCategories();
+        Task<Product> GetItem(int id);
+        Task<Product> GetCategory(int id);
+
+    }
+}
